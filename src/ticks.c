048226362323cc0ba633b4ae86f191cd3387041c
omnitia <83828882+omnit3a@users.noreply.github.com>
2023-10-26 13:28:27 -0400

    Delete src directory

--- src/ticks.c
+++
@@ -1,9 +0,0 @@
-#include <SDL2/SDL.h>
-#include <ticks.h>
-
-int current_tick = 0;
-
-int get_current_tick(){
-  current_tick = (SDL_GetTicks() / (1000 / TICKS_PER_SECOND) % TICKS_PER_SECOND) + 1;
-  return current_tick;
-}
