109b6a412d45c1ee831f50a2a86149a4b13904ac
omnitia <83828882+omnit3a@users.noreply.github.com>
2023-10-28 22:35:51 -0400

    Update version message

--- include/messages.h
+++ include/messages.h
@@ -1,10 +1,7 @@
 #ifndef MESSAGES_H_
 #define MESSAGES_H_
 
-#define CURRENT_VERSION_MSG "Alpha 0.3.5"
+#define CURRENT_VERSION_MSG "Alpha 0.3.6"
 #define CURRENT_BLOCK_MSG "Current Block: "
-#define CURRENT_TIME_MSG "Time: "
-#define DAYTIME_MSG "Day"
-#define NIGHTTIME_MSG "Night"
 
 #endif
