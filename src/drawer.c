bad0fa74b570c87d0217b68b72718ad71e339d27
omnitia <83828882+omnit3a@users.noreply.github.com>
2023-10-30 10:20:44 -0400

    Add files via upload

--- src/drawer.c
+++ src/drawer.c
@@ -32,7 +32,7 @@ void draw_view(render_obj_t * object){
       for (int y = pos.y-(view_y/2) ; y <= pos.y+(view_y/2) ; y++){
 
 	if (is_block_shaded(pos.x, pos.y, pos.z-1)){
-	  drawing_height = pos.z;
+	  drawing_height = pos.z+1;
 	} else {
 	  drawing_height = MAP_HEIGHT;
 	}
